1cca725eba81533b9a6e88f16f126993753005f3
Jack <jcruse_mulhall@stuy.edu>
2017-12-10 20:42:02 -0500

    edited code

--- control.c
+++ control.c
@@ -8,56 +8,116 @@
 #include <unistd.h>
 #include <sys/shm.h>
 #include <sys/ipc.h>
+#include <errno.h>
 
 #define SEM_KEY 23
 #define SHM_KEY 32
 
-int main(int argc, char *argv[]) {
-  int md, val, fd, shmd;
-  char story[250];
+int main(int argc, char ** argv) {
+  int sd, val, fd, shmd;
+  union semun value_passer;
+  char story[256];
+  
+  if (argc < 2) {
+    printf("Too few args...\nTry again next time\n");
+    printf("Maybe try -c N, -v, or -r\n");
+    exit(0);
+  }
 
-  if (argc > 1) {
-    if (!strcmp(argv[1], "-c")) {
-      md = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0664);
-
-      if (md == -1) {
-        printf("semaphore already exists\n");
-        return 0;
+  if (!strcmp(argv[1], "-c")) {
+    
+    if ((sd = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL | 0644)) == -1) {
+      if (!(strcmp(strerror(errno), "File exists"))) {
+	printf("semaphore already exists\n");
+	exit(0);
       }
-      printf("semaphore created: %d\n", md);
-
-      semctl(md, 0, SETVAL, 1);
-      val = semctl(md, 0, GETVAL);
-      printf("value set: %d\n", val);
-
-      shmd = shmget(SHM_KEY, 32, IPC_CREAT | IPC_EXCL);
-
-      fd = open("story", O_CREAT | O_TRUNC, 0666);
-      close(fd);
+      printf("%s\n", strerror(errno));
+      exit(0);
     }
-    else if (!strcmp(argv[1], "-v")) {
-      fd = open("story", O_RDONLY);
-      read(fd, &story, sizeof(story));
-      printf("Story:\n\n%s\n\n", story);
-      close(fd);
+
+    printf("semaphore created: %d\n", sd);
+
+    value_passer.val = 1;
+    
+    if ((semctl(sd, 0, SETVAL, value_passer)) == -1) {
+       printf("%s\n", strerror(errno));
+      exit(0);
     }
-    else if (!strcmp(argv[1], "-r")) {
-      fd = open("story", O_RDONLY);
-      read(fd, &story, sizeof(story));
-      printf("Story:\n\n%s\n\n", story);
-      close(fd);
 
-      shmd = shmget(SHM_KEY, 32, 0);
-      shmctl(shmd, IPC_RMID, 0);
+    if ((val = semctl(sd, 0, GETVAL)) == -1) {
+      printf("%s\n", strerror(errno));
+      exit(0);
+    }
+    printf("value set to: %d\n", val);
 
-      md = semget(SEM_KEY, 1, 0664);
-      val = semctl(md, 0, IPC_RMID);
-      printf("semaphore removed: %d\n", val);
-      execlp("rm", "story", (char *) NULL);
+    shmd = shmget(SHM_KEY, 32, IPC_CREAT | IPC_EXCL);
+
+    fd = open("story", O_CREAT | O_TRUNC, 0666);
+    close(fd);
+  }
+
+  else if (!strcmp(argv[1], "-v")) {
+    if ((fd = open("story", O_RDONLY)) == -1) {
+      printf("%s\n", strerror(errno));
+    }
+    
+    if (read(fd, &story, sizeof(story)) == -1) {
+      printf("%s\n", strerror(errno));
+    }
+    
+    printf("Story:\n\n%s\n\n", story);
+
+    if (close(fd) == -1) {
+      printf("%s\n", strerror(errno));
     }
   }
+
+  else if (!strcmp(argv[1], "-r")) {
+    if ((fd = open("story", O_RDONLY)) == -1) {
+      printf("%s\n", strerror(errno));
+    }
+    
+    if (read(fd, &story, sizeof(story)) == -1) {
+      printf("%s\n", strerror(errno));
+    }
+    
+    printf("Story:\n\n%s\n\n", story);
+
+    if (close(fd) == -1) {
+      printf("%s\n", strerror(errno));
+    }
+    
+    if ((shmd = shmget(SHM_KEY, 32, 0)) == -1) {
+      printf("%s\n", strerror(errno));
+      exit(0);
+    }
+
+    if (shmctl(shmd, IPC_RMID, 0) == -1) {
+      printf("%s\n", strerror(errno));
+      exit(0);
+    }
+
+    printf("shared memory removed\n");
+
+    if ((sd = semget(SEM_KEY, 1, 0)) == -1) {
+      printf("%s\n", strerror(errno));
+      exit(0);
+    }
+
+    if ((semctl(sd, 0, IPC_RMID)) == -1) {
+      printf("%s\n", strerror(errno));
+      exit(0);
+    }
+
+    printf("semaphore removed\n");
+    execlp("rm", "story", (char *) NULL);   
+  }
+
   else {
-    printf("Incorrect arguments\n");
+    printf("HEY! Give me something to work with...\n");
+    printf("Maybe try -c N, -v, or -r\n");
+    exit(0);
   }
-  return 0;
+
 }
+
